788df0c44056898b9eaeec43f66f16b8c9ab0812
“Paul <pauldavidburton@gmail.com>
2023-01-05 19:26:47 -0600

    semantic html commit 5  y'all

--- f1/src/Footer.js
+++ f1/src/Footer.js
@@ -1,8 +1,9 @@
 function Footer () {
-
+return (
     <>
-
+        <img role="logo" src="assets/Rectangle 4.svg"/>
+        <h2 role="Copyright">© Little Lemon</h2>
     </>
-};
+)};
 
 export default Footer;
--- f1/src/Main.js
+++ f1/src/Main.js
@@ -1,17 +1,17 @@
 function Main () {
-
+return (
     <>
-        <>
+        
         <h1 role="Page title">Reserve
         A Table</h1>
         
         <img role="logo" src="assets/Rectangle 4"/>
-        </>
+        
         <>
             <h3></h3>
             
         </>
     </>
-};
+)};
 
 export default Main;
