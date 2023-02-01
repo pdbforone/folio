2d8e2e6a4b3995766a280536b934a64607378ac9
“Paul <pauldavidburton@gmail.com>
2023-02-01 16:29:08 -0600

    added a form and some state

--- f1/src/Components/ReservationForm.js
+++ f1/src/Components/ReservationForm.js
@@ -1,9 +1,27 @@
+import { useState } from "react";
 
-const styles = {display: "grid",maxwidth: "200px", gap: "20px"}
 
 
-const ReservationForm = () => {
+
+function ReservationForm () {
     
+   const [date, SetDate] = useState("");
+   const [time, SetTime] = useState("");
+   const [guests, SetGuests] = useState("");
+   const [wheelch, SetWheelch] = useState("");
+   const [occasion, SetOccasion] = useState("");
+   const styles = {display: "grid", maxwidth: "200px", gap: "20px", color: "black"}
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      SetDate("");
+      SetTime("");
+      SetGuests("");
+      SetWheelch("");
+      SetOccasion("");
+     console.log("Form Submitted!");}
+
+   return (
     <section id= "forms">
     
     <hgroup id="formh1">
@@ -11,11 +29,18 @@ const ReservationForm = () => {
         <h1>we can fit you in!</h1>
     </hgroup>
 
-    <form style ={styles} />
-   <label htmlFor="res-date">Choose date</label>
-   <input type="date" id="res-date"/>
-   <label htmlFor="res-time">Choose time</label>
-   <select id="res-time ">
+    <form style ={styles}>
+   <label htmlFor="date">Choose date</label>
+   <input type="date" 
+   placeholder="Choose Date" 
+   name="date" 
+   value={date} 
+   id="date" 
+   onChange={(e)=>SetDate(e.target.value)}/>
+   <label htmlFor="time">Choose time</label>
+   <select id="time "
+   value={time}
+   onChange={(e)=>SetTime(e.target.value)}>
       <option>17:00</option>
       <option>18:00</option>
       <option>19:00</option>
@@ -24,16 +49,38 @@ const ReservationForm = () => {
       <option>22:00</option>
    </select>
    <label htmlFor="guests">Number of guests</label>
-   <input type="number" placeholder="1" min="1" max="10" id="guests"/>
+   <input type="number"
+    placeholder="1" 
+    min="1" 
+    max="10" 
+    id="guests"
+    value={guests}
+   onChange={(e)=>SetGuests(e.target.value)}
+    />
+   <label htmlFor="wheelch">Number of wheelchairs</label>
+   <input type="number"
+    placeholder="1" 
+    min="1" 
+    max="10" 
+    id="wheelch"
+    value={wheelch}
+   onChange={(e)=>SetWheelch(e.target.value)}
+    />
    <label htmlFor="occasion">Occasion</label>
-   <select id="occasion">
+   <select id="occasion"
+   value={occasion}
+   onChange={(e)=>SetOccasion(e.target.value)}>
       <option>Birthday</option>
       <option>Anniversary</option>
    </select>
-   <input type="submit" value="Make Your reservation"/>
+   <input type="submit"
+   value="Preview Reservation"
+   onSubmit={handleSubmit}
+   />
+   </form>
 </section>
 
-
+   );
 }
 
 
--- f1/src/Components/Reservations.js
+++ f1/src/Components/Reservations.js
@@ -3,19 +3,22 @@ import Lime from '../assets/Rectangle 4.svg';
 
 function Reservations () {
 return (
-    <main  className="content">
-        <section id="hero" >
-        <div className="main-hero">
-        <hgroup id= "heroh1g">
-        <h1 className="reserve">Reserve</h1>
-        <h1 className="reserve">A Table</h1>
-        </hgroup>
+    <>
 
-        <img id="lime" src={Lime} alt= "Lime Logo"/>
-        </div>
+    <main className="content">
+        <section id="hero">
+            <div className="main-hero">
+                <hgroup id="heroh1g">
+                    <h1 className="reserve">Reserve</h1>
+                    <h1 className="reserve">A Table</h1>
+                </hgroup>
+
+                <img id="lime" src={Lime} alt="Lime Logo" />
+            </div>
         </section>
         <ReservationForm/>
     </main>
+    </>
 )};
 
 export default Reservations;
